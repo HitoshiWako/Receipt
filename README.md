3bbdca8d3a9b847edcef4abf6aee6103fc820316
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2024-03-09 15:00:08 +0900

    Image Upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # レシートアプリ
+## 参考サイト
+- [【Django】画像をアップロードする](https://daeudaeu.com/django-upload-image/)
--- mysite/receipt/views.py
+++ mysite/receipt/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import UploadForm
+
+def index(request):
+    params = {
+        'title': '画像のアップロード',
+        'upload_form': UploadForm(),
+        'id': None,
+    }
+
+    if (request.method == 'POST'):
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload_image = form.save()
+
+            params['id'] = upload_image.id
+
+    return render(request, 'receipt/index.html', params)
