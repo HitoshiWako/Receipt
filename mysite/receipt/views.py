548407194bd31eb85f7fa2b1e7ad4d3c270f01e5
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2024-03-10 16:20:41 +0900

    View Card

--- mysite/receipt/views.py
+++ mysite/receipt/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from .forms import UploadForm
+from .models import UploadImage
 
 def index(request):
     params = {
         'title': '画像のアップロード',
         'upload_form': UploadForm(),
         'id': None,
+        'images':None
     }
 
     if (request.method == 'POST'):
@@ -16,5 +18,6 @@ def index(request):
             upload_image = form.save()
 
             params['id'] = upload_image.id
+    params['images'] = UploadImage.objects.order_by('-id')
 
     return render(request, 'receipt/index.html', params)
